b215b447c2d97edf15fbda691817045bb294f169
Hasty Granbery <hasty@granbery.org>
2024-03-26 14:04:10 -0700

    Embed version in build

--- build.sh
+++ build.sh
@@ -1,1 +1,4 @@
-GOOS=linux GOARCH=amd64 go build -o alchemy-disco-linux-x64 -tags github ../alchemy/*.go
+OUTPUT_DIR=$(pwd)
+pushd ../alchemy
+GOOS=linux GOARCH=amd64 go build -ldflags="-X 'github.com/hasty/alchemy/config.Tag=$(git describe --tags)'" -o ${OUTPUT_DIR}/alchemy-disco-linux-x64 -tags github 
+popd
